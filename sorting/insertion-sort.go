9561f2f5be957d4d0aa3d9152ea75eb6c16db0d4
Amitav Mishra <amitav.mishra@gmail.com>
2023-02-24 00:36:58 +0530

    Update insertion-sort.
    Show two variations of the algorithm.  The first one is somewhat
    simplistic, and the second one improves on the first.
    
    We also make use of the newly added utility functions to test our
    implementation.

--- sorting/insertion-sort.go
+++ sorting/insertion-sort.go
@@ -1,24 +1,33 @@
 package main
 
-import (
-	"fmt"
-)
+func main() {
+	testSort(insertionSortV2)
+}
 
-// insertionSort sorts an array of integers into ascending order in place,
-// using the insertion-sort algorithm.
-func insertionSort(arr []int) {
+// Sort an array of integers in-place, using the insertion-sort algorithm.
+// This is a somewhat simplistic implementation of insertion-sort; we will
+// improve on this a little in the next version below.
+func insertionSortV1(arr []int) {
 	for i := 1; i < len(arr); i++ {
-		j, key := i-1, arr[i]
-		for ; j >= 0 && arr[j] > key; j-- {
-			arr[j+1] = arr[j]
+		for j := i - 1; j >= 0 && arr[j] > arr[j+1]; j-- {
+			// Keep shifting the element under consideration (arr[i]) to the
+			// left as long as it's less than the one to its left.
+			arr[j], arr[j+1] = arr[j+1], arr[j]
 		}
-		arr[j+1] = key
 	}
 }
 
-func main() {
-	arr := []int{-2, 44, 36, -19, 0, 58, 7, -69}
-	fmt.Println(arr)
-	insertionSort(arr)
-	fmt.Println(arr)
+// The above version looks ok (and it's expressed concisely in Go), but we can
+// improve on it a bit: instead of swapping the elements, we can keep moving
+// them to the right till we find an element that is <= the element being
+// considered (the "key").  So we effectively do only one copy instead of two
+// in each iteration (of the inner loop).  Ref: CLRS4:19
+func insertionSortV2(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		j, key := i-1, arr[i]
+		for ; j >= 0 && arr[j] > key; j-- {
+			arr[j+1] = arr[j] // Move elements > key one position to the right.
+		}
+		arr[j+1] = key
+	}
 }
